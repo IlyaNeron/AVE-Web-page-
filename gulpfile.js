dc91d85504cfe258e01870a3e61223574f49a9ed
Ilya Neron <ilyaneron@gmail.com>
2018-08-13 16:48:40 +0300

    New page "brand" and was fix x-scroll on main page

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,7 @@ gulp.task('serve', ['watchFiles'], function(){
     });
 
     gulp.watch("assets/css/**/*.scss", ['watchFiles']);
-    gulp.watch(['shop.html', 'index.html', '*.php', 'assets/css/*.css']).on('change', browserSync.reload);
+    gulp.watch(['brand.html', 'index.html', '*.php', 'assets/css/*.css']).on('change', browserSync.reload);
 });
 
 gulp.task("default", ["clean", 'build'], function() {
