d82c42d560ca96010dfd5baa27157b1431b616c0
Ilya Neron <ilyaneron@gmail.com>
2018-08-23 14:16:17 +0300

    Responsive structure, burger-menu, some fixes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,15 @@
-$( document ).ready(function() {
+function addClass () {
+ if (btn.classList.contains('active-button')) {
+   console.log('if');
+   document.body.classList.remove("active-nav");
+   btn.classList.remove('active-button');
+ } else {
+   console.log('else');
+   document.body.classList.add("active-nav");
+   btn.classList.add('active-button');
+ }
+}
 
-  // Get started!
+var btn = document.querySelector('.burger-menu-button');
 
-});
+btn.addEventListener('click', addClass);
